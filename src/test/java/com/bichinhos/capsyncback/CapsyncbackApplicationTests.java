658c32358f5ed3018c63f97a9931262f6cd0f307
GustavonBrito <gustavoserafio@gmail.com>
2024-06-16 22:09:36 -0300

    chore:adicionando dependencias + docker compose no projeto + Readme

---
+++ src/test/java/com/bichinhos/capsyncback/CapsyncbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bichinhos.capsyncback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsyncbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
