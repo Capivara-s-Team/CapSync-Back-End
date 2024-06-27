34b7c2810db683aec9a39b4bedb1f489503408d9
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-06-26 21:34:25 -0300

    feat: adicionar validação para verificar se o name é unico antes de criar uma Squad

--- src/main/java/com/bichinhos/CapSync_Back_End/repository/ISquadRepository.java
+++ src/main/java/com/bichinhos/CapSync_Back_End/repository/ISquadRepository.java
@@ -4,6 +4,11 @@ import com.bichinhos.CapSync_Back_End.entity.SquadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ISquadRepository extends JpaRepository<SquadEntity, Long> {
+
+    Optional<SquadEntity> findByName(String name);
+
 }
