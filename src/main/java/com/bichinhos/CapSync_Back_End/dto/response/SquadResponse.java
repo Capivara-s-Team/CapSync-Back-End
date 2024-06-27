8e580811fc7ed21970da81fb0e134b90f1db1fe2
Claudio Netto Junior <115486214+claudionetto@users.noreply.github.com>
2024-06-26 23:08:25 -0300

    Revert "Feat/squad"

--- src/main/java/com/bichinhos/CapSync_Back_End/dto/response/SquadResponse.java
+++
@@ -1,4 +0,0 @@
-package com.bichinhos.CapSync_Back_End.dto.response;
-
-public record SquadResponse (Long id, String name) {
-}
--- src/main/java/com/bichinhos/CapSync_Back_End/dto/response/StackResponse.java
+++
@@ -1,4 +0,0 @@
-package com.bichinhos.CapSync_Back_End.dto.response;
-
-public record StackResponse (Long id, String name) {
-}
